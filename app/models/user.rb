1c111e20d30f1bcfd710d11049ed21dd08c7d344
fernal9301 <ivanovsergey1988@gmail.com>
2017-05-31 00:46:47 +0300

    install and configure devise to work with project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :events
 
   validates :name, presence: true, length: {maximum: 35}
