b108dc0c0af619668b3b280fd330c14677a48224
fernal9301 <ivanovsergey1988@gmail.com>
2017-06-01 21:40:59 +0300

    hide edit btn from user profile if it another one

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   def set_current_user
     @user = current_user
   end
-  
+
   def user_params
     params.require(:user).permit(:name, :email)
   end
