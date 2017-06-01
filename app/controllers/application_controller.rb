a6593fe8d156561adb5519dacd62d3e11c85521f
fernal9301 <ivanovsergey1988@gmail.com>
2017-06-01 21:12:08 +0300

    add current_user_can_edit? to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
       keys: [:password, :password_confirmation, :current_password]
     )
   end
+
+  def current_user_can_edit?(event)
+    user_signed_in? && event.user == current_user
+  end
 end
