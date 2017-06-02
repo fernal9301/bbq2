76162bd6e91f9591644d79d1125f1ecc38b05b6d
Sergey Ivanov <ivanovsergey1988@gmail.com>
2017-06-02 08:32:19 +0000

    set :ru locale to check heroku

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Bbq
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ru
+    config.i18n.locale = :ru
   end
 end
