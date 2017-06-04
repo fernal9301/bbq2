47332ca6739e4d636e3876730accd055825931ac
fernal9301 <ivanovsergey1988@gmail.com>
2017-06-04 20:04:00 +0300

    add comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :events
+  has_many :comments
 
   validates :name, presence: true, length: {maximum: 35}
 
