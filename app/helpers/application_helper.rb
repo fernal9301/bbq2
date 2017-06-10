84af8a4ff6154aea40d3843f5b9eb66d7919ab9e
fernal9301 <ivanovsergey1988@gmail.com>
2017-06-10 10:16:50 +0300

    add photo feature to event

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,35 @@ module ApplicationHelper
       asset_path('user.png')
     end
   end
+# Аналогично user_avatar, только возвращает миниатюрную версию
+  def user_avatar_thumb(user)
+    if user.avatar.file.present?
+      user.avatar.thumb.url
+    else
+      asset_path('user.png')
+    end
+  end
+
+  # Возвращает адерс рандомной фотки события, если есть хотя бы одна. Или ссылку
+  # на дефолтную картинку.
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      asset_path('event.jpg')
+    end
+  end
+
+  # Аналогично event_photo, только возвращает миниатюрную версию
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      asset_path('event_thumb.jpg')
+    end
+  end
  end
